58092573e0faebf5b9e8e2fecc87b83a26c6d3a9
jjoon9 <wnsrnek3@gmail.com>
2017-05-01 00:17:01 +0900

    init

--- 2959_거북이.cpp
+++
@@ -1,8 +0,0 @@
-#include <cstdio>
-#include <algorithm>
-int main() {
-	int num[4]; scanf("%d%d%d%d", &num[0],&num[1],&num[2],&num[3]);
-	std::sort(num,num+4);
-	printf("%d", num[0] * num[2]);
-	return 0;
-}
